05c3f6268e29be3e62c4ea8d6caeb320a17ba6c4
Guruprasad <guruprasadss2004@gmail.com>
2025-08-09 20:20:32 +0530

    Update AdminController.java

--- src/main/java/org/example/placement_management/Controller/AdminController.java
+++ src/main/java/org/example/placement_management/Controller/AdminController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RequestMapping("/Admin")
-@RestController
+@RestController  //adminController
 public class AdminController  {
     @Autowired
     private AdminService adminService;
