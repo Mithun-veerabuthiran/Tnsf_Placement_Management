4ac6c4b2819d436b519fcb277525bf5f93751e78
C.C.RAGOUNATHE <ragounathe007@gmail.com>
2025-08-09 20:12:19 +0530

    Userrepo.java

--- src/main/java/org/example/placement_management/repo/Userrepo.java
+++ src/main/java/org/example/placement_management/repo/Userrepo.java
@@ -4,4 +4,4 @@ import org.example.placement_management.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface Userrepo extends JpaRepository<User,Integer> {
-}
+} // repo
